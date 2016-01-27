37449ae7d412c8543a2d82944d2124dcfed0157c
Vladimir Shevchuk <dosandkv@gmail.com>
2016-01-27 21:26:10 +0700

    add gulp task for eslint;

--- gulpfile.js
+++ gulpfile.js
@@ -4,10 +4,23 @@ var mainBowerFiles = require('main-bower-files');
 var compass = require('gulp-compass');
 var concat = require('gulp-concat');
 var requirejsOptimize = require('gulp-requirejs-optimize');
+var eslint = require('gulp-eslint');
 
-
-gulp.task('default', function() {
+gulp.task('default', ['lint'], function() {
     console.error('default task');
+});
+
+gulp.task('lint', function () {
+    return gulp.src(['app/js/**/*.js'])
+        // eslint() attaches the lint output to the eslint property
+        // of the file object so it can be used by other modules.
+        .pipe(eslint())
+        // eslint.format() outputs the lint results to the console.
+        // Alternatively use eslint.formatEach() (see Docs).
+        .pipe(eslint.format())
+        // To have the process exit with an error code (1) on
+        // lint error, return the stream and pipe to failOnError last.
+        .pipe(eslint.failOnError());
 });
 
 gulp.task('runLocalServer', function() {
