41e6d582761f8a736b4d2ba821b4eb942fa87a44
Vladimir Shevchuk <dosandkv@gmail.com>
2016-02-16 17:33:11 +0200

    added auto-deploy to gh-pages for project;

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,4 @@
 var gulp = require('gulp');
-var mainBowerFiles = require('main-bower-files');
 var compass = require('gulp-compass');
 var concat = require('gulp-concat');
 var requirejsOptimize = require('gulp-requirejs-optimize');
@@ -7,9 +6,7 @@ var eslint = require('gulp-eslint');
 var minify = require('gulp-cssnano');
 var preprocess = require('gulp-preprocess');
 var clean = require('gulp-clean');
-
 var connect = require('gulp-connect');
-var modRewrite = require('connect-modrewrite');
 
 var preprocessConfig = {
     context: {
@@ -134,87 +131,3 @@ gulp.task('compile-scss', function() {
 gulp.task('compass-watch', function() {
     gulp.watch('app/scss/**/*.scss', ['compile-scss']);
 });
-
-gulp.task('main-js-core', ['main-js-backbone', 'main-js-requirejs', 'main-js-core-requirejs-plugins']);
-
-gulp.task('main-js-backbone', function() {
-    return gulp.src(mainBowerFiles(['**/backbone/*.js', '**/underscore/*.js', '**/jquery.js']))
-        .pipe(gulp.dest('./app/js/libs2'));
-});
-
-gulp.task('main-js-requirejs', function() {
-    return gulp.src(mainBowerFiles(['**/requirejs/*.js']))
-        .pipe(gulp.dest('./app/js/libs2'));
-});
-
-gulp.task('main-js-core-requirejs-plugins', function() {
-    return gulp.src(mainBowerFiles(['**/text.js']))
-        .pipe(gulp.dest('./app/js/libs2/requirejs-plugins'));
-});
-
-/*install backgrid*/
-gulp.task('main-backgrid', ['main-backgrid-js', 'main-backgrid-css']);
-
-gulp.task('main-backgrid-js', ['main-js-backgrid', 'main-js-backgrid-filter', 'main-js-backgrid-paginator']);
-
-gulp.task('main-backgrid-css', ['main-css-backgrid', 'main-css-backgrid-filter', 'main-css-backgrid-paginator']);
-
-gulp.task('main-js-backgrid', function() {
-    return gulp.src(mainBowerFiles(['**/backgrid.js']))
-        .pipe(gulp.dest('./app/js/libs2/backbone-plugins/backgrid'));
-});
-
-gulp.task('main-css-backgrid', function() {
-    return gulp.src(mainBowerFiles(['**/backgrid.css']))
-        .pipe(gulp.dest('./app/css/libs/backbone-plugins/backgrid'));
-});
-
-gulp.task('main-js-backgrid-filter', function() {
-    return gulp.src(mainBowerFiles(['**/backgrid-filter.js']))
-        .pipe(gulp.dest('./app/js/libs2/backbone-plugins/backgrid'));
-});
-
-gulp.task('main-css-backgrid-filter', function() {
-    return gulp.src(mainBowerFiles(['**/backgrid-filter.js']))
-        .pipe(gulp.dest('./app/css/libs/backbone-plugins/backgrid'));
-});
-
-gulp.task('main-js-backgrid-paginator', function() {
-    return gulp.src(mainBowerFiles(['**/backgrid-paginator.js']))
-        .pipe(gulp.dest('./app/js/libs2/backbone-plugins/backgrid'));
-});
-
-gulp.task('main-css-backgrid-paginator', function() {
-    return gulp.src(mainBowerFiles(['**/backgrid-paginator.css']))
-        .pipe(gulp.dest('./app/css/libs/backbone-plugins/backgrid'));
-});
-/* install backgrid */
-
-/* START: install bootstrap to project */
-gulp.task('main-bootstrap', ['main-js-bootstrap', 'main-scss-bootstrap-main', 'main-fonts-bootstrap']);
-
-gulp.task('main-js-bootstrap', function() {
-    return gulp.src(mainBowerFiles(['**/bootstrap/*.js', '**/bootstrap.js']))
-        .pipe(gulp.dest('./app/js/libs2/bootstrap'));
-});
-
-gulp.task('main-scss-bootstrap-main', ['main-scss-bootstrap'], function() {
-    return gulp.src(mainBowerFiles(['**/_bootstrap.scss']))
-        .pipe(gulp.dest('./app/scss/libs/bootstrap'));
-});
-
-gulp.task('main-scss-bootstrap', ['main-scss-bootstrap-mixins'], function() {
-    return gulp.src(mainBowerFiles(['**/bootstrap/*.scss']))
-        .pipe(gulp.dest('./app/scss/libs/bootstrap/bootstrap'));
-});
-
-gulp.task('main-scss-bootstrap-mixins', function() {
-    return gulp.src(mainBowerFiles(['**/bootstrap/mixins/*.scss']))
-        .pipe(gulp.dest('./app/scss/libs/bootstrap/bootstrap/mixins'));
-});
-
-gulp.task('main-fonts-bootstrap', function() {
-    return gulp.src(mainBowerFiles('**/bootstrap/*.{eot,svg,ttf,woff,woff2}'))
-        .pipe(gulp.dest('./app/fonts/libs/bootstrap'));
-});
-/* END: install bootstrap to project */
