31119ada3807853f2edb59aee49254028c13d218
Vladimir Shevchuk <dosandkv@gmail.com>
2016-02-17 10:48:41 +0200

    fixed deploy-script;

--- deploy-ghpages.sh
+++ deploy-ghpages.sh
@@ -5,9 +5,9 @@ gulp build
 ( cd out
  git init
  git config user.name "Travis-CI"
- git config user.email "travis@nodemeatspace.com"
- cp ../CNAME ./CNAME
- cp ../countryiso.js ./countryiso.js
+ git config user.email "dosandkv@gmail.com"
+ #cp ../CNAME ./CNAME
+ #cp ../countryiso.js ./countryiso.js
  git add .
  git commit -m "Deployed to Github Pages"
  git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:gh-pages > /dev/null 2>&1
