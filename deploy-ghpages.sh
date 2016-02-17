2edccdde3690b96b2020c75392505be6d496485a
Vladimir Shevchuk <dosandkv@gmail.com>
2016-02-17 12:16:26 +0200

    fixed deploy-script;

--- deploy-ghpages.sh
+++ deploy-ghpages.sh
@@ -1,14 +1,19 @@
-#!/bin/bash
+#!/usr/bin/env bash
+# Get to the Travis build directory, configure git and clone the repo
+cd $HOME
 rm -rf out || exit 0;
-mkdir out;
+mkdir out
+cd out
+git init
+git config user.name "Travis-CI"
+git config user.email "dosandkv@gmail.com"
+git clone --quiet --branch=gh-pages https://${GH_TOKEN}@${GH_REF} gh-pages > /dev/null
+
+# Commit and Push the Changes
+cd gh-pages
+git rm -rf ./dist
+npm install
 gulp build
-( cd out
- git init
- git config user.name "Travis-CI"
- git config user.email "dosandkv@gmail.com"
- npm install
- gulp build
- git add .
- git commit -m "Deployed to Github Pages"
- git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:gh-pages > /dev/null 2>&1
-)
+git add -f .
+git commit -m "Deployed to Github Pages"
+git push -fq origin gh-pages > /dev/null
