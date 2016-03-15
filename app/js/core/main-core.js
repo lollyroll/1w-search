f3c963f168b00939e459fb0ed2fc147ddac6b2e7
Taras Bilozir <bilozir@1worldonline.com>
2016-03-15 13:17:34 +0200

    Task #8561 'Search for existing 1W data: add the flags of the countries in the drop-down list': added flags of the countries

--- app/js/core/main-core.js
+++ app/js/core/main-core.js
@@ -8,7 +8,7 @@ require.config({
         jquery: 'js/core/libs/jquery-2.2.0',
         underscore: 'js/core/libs/underscore-1.6.0',
         backbone: 'js/core/libs/backbone-amd-1.1.0',
-        text: 'js/core/libs/requirejs-plugins/text-2.0.12',
+        text: 'js/core/libs/requirejs-plugins/text-2.0.12'
     }
 });
 
